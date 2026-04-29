cecd170cc1221fad21b6c863e89e7e71bf9954a5
Rishabh-cgi1396 <rishabh.anand@cgi.com>
2026-04-29 10:55:00 +0000

    commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Docker Demo Working 🚀');
+  res.send('Docker Demo is working');
 });
 
 app.get('/health', (req, res) => {
